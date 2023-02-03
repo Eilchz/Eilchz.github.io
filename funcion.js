0251b1fd109165451f35a39d666339271286cfb9
eilchz <107214170+Eilchz@users.noreply.github.com>
2023-02-03 14:41:32 -0300

    Update funcion.js

--- funcion.js
+++ funcion.js
@@ -1,40 +1,8 @@
-function IsNumeric(n) {
-  return !isNaN(n);
-}
-
-$(function () {
-  $("#getit").click(function () {
-    var numLow = $("#lownumber").val();
-    var numHigh = $("#highnumber").val();
-
-    var adjustedHigh = parseFloat(numHigh) - parseFloat(numLow) + 1;
-
-    var numRand = Math.floor(Math.random() * adjustedHigh) + parseFloat(numLow);
-
-    if (
-      IsNumeric(numLow) &&
-      IsNumeric(numHigh) &&
-      parseFloat(numLow) <= parseFloat(numHigh) &&
-      numLow != "" &&
-      numHigh != ""
-    ) {
-      $("#randomnumber").text(numRand);
-    } else {
-      $("#randomnumber").text("Careful now...");
-    }
-
-    return false;
-  });
-
-  $("input[type=text]").each(function () {
-    $(this).data("first-click", true);
-  });
-
-  $("input[type=text]").focus(function () {
-    if ($(this).data("first-click")) {
-      $(this).val("");
-      $(this).data("first-click", false);
-      $(this).css("color", "black");
-    }
-  });
-});
+const number = document.getElementById('number');
+const generateBtn = document.getElementById('generateBtn');
+const randomNumberGenerator = () => {
+  const randomNumber = Math.floor(Math.random() * 10 + 1);
+  number.textContent = randomNumber;
+};
+generateBtn.addEventListener('click', randomNumberGenerator);
+randomNumberGenerator();
