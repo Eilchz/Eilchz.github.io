f98fa5c28f2d98ebc5f9634ce07faa97a98af26f
eilchz <107214170+Eilchz@users.noreply.github.com>
2023-02-03 21:00:01 -0300

    Add files via upload

--- funcion.js
+++ funcion.js
@@ -1,8 +1,47 @@
-const number = document.getElementById('number');
-const generateBtn = document.getElementById('generateBtn');
-const randomNumberGenerator = () => {
-  const randomNumber = Math.floor(Math.random() * 10 + 1);
-  number.textContent = randomNumber;
-};
-generateBtn.addEventListener('click', randomNumberGenerator);
-randomNumberGenerator();
+
+const buttonA = document.querySelector('#Puerta1');
+
+buttonA.onclick = () => {
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+var number;
+number = (getRandomInt(100));
+if(number >= 30){
+    alert("Cabra " + number);
+}
+if(number <= 30){
+    alert("Auto "+ number);
+}
+}
+
+const buttonB = document.querySelector('#Puerta2');
+buttonB.onclick = () => {
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+var number;
+number = (getRandomInt(100));
+if(number >= 30){
+    alert("Cabra " + number);
+}
+if(number <= 30){
+    alert("Auto "+ number);
+}
+}
+
+const buttonC = document.querySelector('#Puerta3');
+buttonC.onclick = () => {
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+var number;
+number = (getRandomInt(100));
+if(number >= 30){
+    alert("Cabra " + number);
+}
+if(number <= 30){
+    alert("Auto "+ number);
+}
+}
+
